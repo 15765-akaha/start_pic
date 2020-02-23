4da8684aaa3b2d1ec3eb73cddc28a56098582b0e
15765-akaha <turning2012@gmail.com>
2020-02-23 03:26:50 +0000

    userモデルメソッド設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # バリデーション
+  validates :name, presence: true, length:{ minimum:2, maximum: 20}
+  validates	:introduction, length: { maximum: 150 }
+  # refaile
+  attachment :profile_image
+  # 称号設定
+  enum :accivement { beginner: 0, rookie: 1, challenger: 2, advanced: 3, master: 4}
+  # リレーション
+  has_many :post_images, dependent: :destroy
+  has_many :likes,		 dependent: :destroy
+  # ユーザーがお気に入りした投稿画像を集める
+  has_many :likes_post_images, through: :likes, source: :post_images
+  has_many :image_comments, dependent: :destroy
+  # フォローする側のユーザー視点
+  has_many :active_relationships, class_name:"Relationship", foreign_key:"following_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :follower
+  # フォローされる側のユーザー視点
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :following
+
+
 end
