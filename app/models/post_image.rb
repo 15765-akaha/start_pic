790a2561fdd0a01570004167ef3826184d00136d
15765-akaha <turning2012@gmail.com>
2020-02-23 03:46:21 +0000

    PostImageモデル attachmentメソッド追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,8 +1,8 @@
 class PostImage < ApplicationRecord
   validates :image_id, presence: true
+  attachment :image
   belongs_to :user
   belongs_to :theme
-
   has_many :image_comments, dependent: :destroy
   has_many :likes,			dependent: :destroy
 end
