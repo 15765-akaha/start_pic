a19a0643750c6755db729dfc2e221c185cfd1150
15765-akaha <turning2012@gmail.com>
2020-02-25 00:26:25 +0000

    themeの作成・削除・一覧機能追加

--- app/controllers/admins/themes_controller.rb
+++ app/controllers/admins/themes_controller.rb
@@ -1,11 +1,23 @@
 class Admins::ThemesController < ApplicationController
   def index
+    @theme = Theme.new
+    @themes = Theme.all.order(:title)
   end
 
   def create
+    @theme = Theme.new(theme_params)
+    if @theme.save
+       redirect_to admins_themes_path
+    else
+       @themes = Theme.all.order(:title)
+       render :index
+    end
   end
 
   def destroy
+    @theme = Theme.find(params[:id])
+    @theme.destroy
+    redirect_to admins_themes_path
   end
 
   def edit
@@ -13,4 +25,9 @@ class Admins::ThemesController < ApplicationController
 
   def update
   end
+
+  private
+   def theme_params
+        params.require(:theme).permit(:title, :is_enabled)
+   end
 end
