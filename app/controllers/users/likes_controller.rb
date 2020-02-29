572f92c22ee34e21b294a3b55cefdcd1324bf2c3
15765-akaha <turning2012@gmail.com>
2020-02-29 12:13:35 +0000

    いいね非同期対応

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -4,13 +4,11 @@ class Users::LikesController < ApplicationController
     @post_image = PostImage.find(params[:post_image_id])
     like = current_user.likes.new(post_image_id: params[:post_image_id])
     like.save
-    redirect_back(fallback_location: root_path)
   end
     
   def destroy
     @post_image = PostImage.find(params[:post_image_id])
     like = current_user.likes.find_by(post_image_id: params[:post_image_id])
     like.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
