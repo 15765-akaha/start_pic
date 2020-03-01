7fe6ad36c4aa25f81e9a1a2dc7bb00364e15be44
15765-akaha <turning2012@gmail.com>
2020-03-01 05:02:14 +0000

    コメント削除機能追加

--- app/controllers/users/image_comments_controller.rb
+++ app/controllers/users/image_comments_controller.rb
@@ -1,16 +1,23 @@
 class Users::ImageCommentsController < ApplicationController
     before_action :authenticate_user!
     def create
+      @post_image = PostImage.find(params[:post_image_id])
       @image_comment = current_user.image_comments.new(image_comment_params)
+      @image_comment.post_image_id = @post_image.id
+      @image_comment.save
+      redirect_to post_image_path(@post_image)
     end
   
     def destroy
+      @post_image = PostImage.find(params[:post_image_id])
+      @image_comment = current_user.image_comments.find_by(id: params[:id],post_image_id: params[:post_image_id])
+      @image_comment.destroy
+      redirect_to post_image_path(@post_image)
     end
 
     private
-    # def image_commnet_params
-    #     require(:image_comment).permit()
-    # end
+    def image_comment_params
+        params.require(:image_comment).permit(:comment)
+    end
 
-  end
 end
