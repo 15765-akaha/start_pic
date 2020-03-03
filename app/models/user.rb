f0bbeba94d26232e9929fe295137db225cb38cbe
15765-akaha <turning2012@gmail.com>
2020-03-03 10:26:13 +0000

    称号名変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # refaile
   attachment :profile_image
   # 称号設定 0=beginner
-  enum accivement: [:beginner, :rookie, :challenger, :advanced, :master]
+  enum accivement: [:BEGINNER, :ROOKIE, :CHALLENGER, :ADVANCED, :MASTER]
   # リレーション
   has_many :post_images, dependent: :destroy
   has_many :likes,		 dependent: :destroy
