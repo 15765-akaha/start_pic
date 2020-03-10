c1a7a25caad39070adca017b63f9204a33d53b50
15765-akaha <turning2012@gmail.com>
2020-03-10 11:26:37 +0000

    称号画像ヘルパーメソッド定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def achivement_image_name(achivement) # ユーザーの称号に応じて画像を変更する。
+        case achivement
+        when "BEGINNER"
+         'begginer.png'
+        when "ROOKIE"
+         'main.jpg'
+        when "CHALLENGER"
+         'begginer.png'
+        when "MASTER"
+         'begginer.png'
+        end 
+    end       
 end
