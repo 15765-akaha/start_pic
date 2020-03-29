9c52a0b69e0b01acba34e8c9e75c860d9e421e4a
15765-akaha <turning2012@gmail.com>
2020-03-29 15:26:16 +0000

    faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name {"test"}
-    sequence(:email) { |n| "test#{n}@example.com"}
-    password {"password"}
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    introduction { Faker::Lorem.characters(number:20) }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
