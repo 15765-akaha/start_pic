0adb2dc230d7b16a7978689ff2b8ea74cbf55f66
15765-akaha <turning2012@gmail.com>
2020-04-01 03:02:21 +0000

    テスト記述追加

--- spec/factories/themes.rb
+++ spec/factories/themes.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :theme do
     title { Faker::Lorem.characters(number:5)}
+    
   end
 end
--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -13,26 +13,30 @@ RSpec.describe "テーマモデルのテスト", type: :model do
       it "タイトルが入力されているので作成できる" do
         expect(@theme).to be_valid
       end
+      it "is_enabled（公開/非公開)がtrueで作成されている" do
+        expect(@theme.is_enabled).to eq true
+      end
+      it "is_enabled(公開/非公開）がfalseでも作成できる" do
+        @theme.is_enabled = false
+        expect(@theme).to be_valid
+      end
+      it "adminフラグがtrueで作成されている" do
+        expect(@theme.admin).to eq true
+      end
+      it "create_user_idがnillで作成されている" do
+        expect(@theme.create_user_id).to eq nil
+      end
 
       it "タイトルが入力されていないので作成できない" do
         @theme.title = nil
         expect(@theme).to_not be_valid
       end
       it "タイトルが重複しているため作成できない" do
-        theme1 = create(:theme)
+        theme1 = create(:theme, admin: true)
         @theme.title = theme1.title
         expect(@theme).to_not be_valid
       end
-      it "is_enabled（公開/非公開)がtrueで作成されている" do
-        expect(@theme.is_enabled).to eq true
-      end
-      it "adminフラグがtrueで作成されている" do
-        @theme.admin = true
-        expect(@theme.admin).to eq true
-      end
-      it "create_user_idがnillで作成されている" do
-        expect(@theme.create_user_id).to eq nil
-      end
+
     end
 
     context "ユーザーが作成するパターン" do
@@ -50,7 +54,7 @@ RSpec.describe "テーマモデルのテスト", type: :model do
         expect(@theme).to_not be_valid
       end
       it "タイトルが重複しているため作成できない" do
-        theme1 = create(:theme)
+        theme1 = create(:theme, create_user_id:@user.id)
         @theme.title = theme1.title
         expect(@theme).to_not be_valid
       end
@@ -62,7 +66,7 @@ RSpec.describe "テーマモデルのテスト", type: :model do
         expect(@theme.create_user_id).to eq @user.id
       end
 
-      it "adminがfalseの状態でcreate_user_idがない場合は無効である" do
+      it "adminがfalseの状態でcreate_user_idがない場合は作成できない" do
         @theme.create_user_id = nil
         expect(@theme).to_not be_valid
       end
