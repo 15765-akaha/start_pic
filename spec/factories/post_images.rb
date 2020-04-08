b159f9ff20c20ef670196497d08e3c925286613b
15765-akaha <turning2012@gmail.com>
2020-04-08 17:45:50 +0000

    投稿画像テスト追記

--- spec/factories/post_images.rb
+++ spec/factories/post_images.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :post_image do
-    image_id { Rack::Test::UploadedFile.new( File.join( Rails.root, 'spec/fixtures/test.jpg' ) ) }
     caption { Faker::Lorem.characters(number:20) }
   end
 end
